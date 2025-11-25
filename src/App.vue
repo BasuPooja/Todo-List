78c040e8085991f5c1ab69bbb73383ba96ad8c61
Pooja Kumari <basupooja306@gmail.com>
2025-11-25 11:35:22 +0530

    Text Interpolation

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       Count is: {{ count }}
     </button>
   </div>
-</template>
+</template> -->
 
-<script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -16,7 +16,7 @@ export default {
 };
 </script> -->
 
-<style>
+<!-- <style>
 button {
   padding: 10px;
   font-size: 18px;
@@ -35,5 +35,41 @@ export default {
 
 <template>
   <button @click="count++">Count is: {{ count }}</button>
+</template> -->
+
+<template>
+  <div class="card">
+    <h2>Text Interpolation</h2>
+
+    <p>Message: <strong>{{ message }}</strong></p>
+    <p>Expression example: <strong>{{ count + 1 }}</strong></p>
+
+    <button style="height: 30px; " @click="count++">
+      Increment (count is {{ count }})
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Pooja, Vue is working!",
+      count: 0
+    };
+  }
+};
+</script>
+
+<style>
+.card {
+  border: 1px solid #ccc;
+  padding: 15px;
+  margin: 30px auto;
+  width: 400px;
+  border-radius: 8px;
+  font-family: Arial;
+}
+</style>
+
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
 
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// createApp(App).mount('#app')
+createApp(App).mount('#app')
 
 /* --------------Global Build --------------*/
 
@@ -19,12 +19,24 @@ import './assets/main.css'
 
 /* --------------ES Module Build --------------*/
 
-const { createApp } = Vue
+// const { createApp } = Vue
 
-createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
-}).mount('#app')
+// createApp({
+//   data() {
+//     return {
+//       message: 'Hello Vue!'
+//     }
+//   }
+// }).mount('#app')
+
+
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello Pooja, Vue is working!",
+//       count: 0
+//     };
+//   }
+// });
+
+// app.mount("#app");
